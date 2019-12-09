9b99a9fbed581780cfcba050afc0f09a3337ddfc
fricchiuti <fricchiuti@bol.com>
2019-12-09 16:28:41 +0100

    add proper attribute value

--- dags/exercise6.py
+++ dags/exercise6.py
@@ -1,11 +1,9 @@
 """Example DAG demonstrating the usage of the BashOperator."""
 
 from datetime import timedelta
-from datetime import datetime
 
 import airflow
 from airflow.models import DAG
-from airflow.hooks.postgres_hook import PostgresHook
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
 
 args = {
@@ -20,13 +18,13 @@ dag = DAG(
     dagrun_timeout=timedelta(minutes=60),
 )
 
-postgresHook = PostgresToGoogleCloudStorageOperator(conn_name_attr='postgres_training_id',
+go = PostgresToGoogleCloudStorageOperator(postgres_conn_id='postgres_training_id',
                                                     sql='SELECT transfer_date FROM land_registry_price_paid_uk LIMIT 30',
                                                     bucket='airflow_training_bucket',
                                                     filename='tst.json',
                                                     task_id='postgres_hook',
                                                     dag=dag)
 
-postgresHook
+go
 
 
