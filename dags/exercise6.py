602060dccf3737403a4c6be8938e08c5d0128011
fricchiuti <fricchiuti@bol.com>
2019-12-09 15:58:06 +0100

    proper name in import

--- dags/exercise6.py
+++ dags/exercise6.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.utils.decorators import apply_defaults
-from airflow.contrib.operators.sql_to_gcs import BaseSQLToGoogleCloudStorageOperator
-from airflow.contrib.operators.sql_to_gcs import PostgresToGoogleCloudStorageOperator
+from airflow.contrib.operators.postgres_to_gcs_operator import BaseSQLToGoogleCloudStorageOperator
+from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
 
 args = {
     'owner': 'Airflow',
