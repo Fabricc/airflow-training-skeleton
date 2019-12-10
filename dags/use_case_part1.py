4b42a0b73e6b084c742b9f8fb5f49925ddae6305
fricchiuti <fricchiuti@bol.com>
2019-12-10 16:02:54 +0100

    implementation first part use case

--- dags/use_case_part1.py
+++ dags/use_case_part1.py
@@ -5,17 +5,18 @@ from dags.http_to_gcs_operator import HttpToGcsOperator
 
 args = {"owner": "godatadriven", "start_date": airflow.utils.dates.days_ago(1)}
 dag = DAG(
-    dag_id="fetch_exchange_rates",
+    dag_id="track_prices",
     default_args=args,
     schedule_interval="0 0 * * *",
 )
 
 fetch_exchange_rates = HttpToGcsOperator(
     endpoint=f"https://api.exchangeratesapi.io/history?start_at={airflow.utils.dates.days_ago(1)}&end_at={airflow.utils.dates.days_ago(0)}&symbols=EUR&base=GBP",
-        gcs_bucket="airflow_training_bucket",
-        gcs_path="bucket",
-        method="GET",
-        http_conn_id="http_default",
-        gcs_conn_id="google_cloud_default",)
+    gcs_bucket="airflow_training_bucket",
+    task_id="fetch_exchange_rates",
+    gcs_path="bucket",
+    method="GET",
+    http_conn_id="http_default",
+    gcs_conn_id="google_cloud_default",)
 
 fetch_exchange_rates
